6c01d56554ece6772207484b63bfcf61ac6d6179
TheDudeFromCI <thedudefromci@gmail.com>
2022-07-13 07:51:49 -0700

    Created AwgenCore prefab.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- Runtime/AwgenCore.cs
+++ Runtime/AwgenCore.cs
@@ -1,15 +1,41 @@
+using System;
 using UnityEngine;
 
 namespace AwgenCore
 {
+  /// <summary>
+  /// The main connector for loading and managing the Awgen Core framework. This
+  /// behaviour should be place on a top-level, empty game object with no other
+  /// components or children. This object should then be placed on every scene.
+  /// Awgen Core will manage redundency and initialization from there.
+  /// </summary>
   public class AwgenCore : MonoBehaviour
   {
     private static bool initialized;
 
-    void Awake()
+
+    [Header("Logic Server")]
+
+    [SerializeField, Range(1, 100)]
+    [Tooltip("The number of game ticks that occur each second within the logic thread.")]
+    private int gameTickRate = 20;
+
+
+    private LogicServer logicServer;
+
+
+    /// <summary>
+    /// Called when the game object is first initialized to setup AwgenCore
+    /// specific handlers, registry, and servers/processes as needed.
+    /// </summary>
+    protected void Awake()
     {
       if (initialized) return;
+      DontDestroyOnLoad(gameObject);
       initialized = true;
+
+      var workers = Math.Max(SystemInfo.processorCount - 1, 1);
+      this.logicServer = new LogicServer(this.gameTickRate, workers);
 
       Voxel.BlockRegistry.Initialize();
     }
